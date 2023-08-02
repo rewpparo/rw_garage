8f15eb26edd565d68c7ff43d81af631ddfcb2011
rewpparo <5676004+rewpparo@users.noreply.github.com>
2023-08-02 15:31:30 +0200

    Removed dump function used for debug

--- server.lua
+++ server.lua
@@ -1,19 +1,6 @@
 
 Garages = {}
 
-function dump(o)
-    if type(o) == 'table' then
-       local s = '\n{ '
-       for k,v in pairs(o) do
-          if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
-       end
-       return s .. '} \n'
-    else
-       return tostring(o)
-    end
-end
-
 -----------------------------------
 -- REGISTER / UNREGISTER GARAGES --
 --       Those are exports       --
